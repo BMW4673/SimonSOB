8bae69c6f01dd4b5b8e4fe9dae997114ae5d0bc1
Brett <58869562+BMW4673@users.noreply.github.com>
2025-12-09 21:00:04 -0600

    Weekly sync: 2025-12-09 21:00 CST

--- GBA472 Assignment 3/code.py
+++ GBA472 Assignment 3/code.py
@@ -18,12 +18,19 @@ df_clean = df_clean.astype({
     'Res': bool,
     'Hours24': bool
 })
+df_clean['Res'] = df_clean['Res'].apply(lambda x: 1 if x > 0 else 0)
+df_clean['Hours24'] = df_clean['Hours24'].apply(lambda x: 1 if x > 0 else 0)
 
-
-# Formatting for Display
+# Formatting for Display and Print
 df_display = df_clean.copy()
 df_display['Sales'] = df_display['Sales'].apply(lambda x: f"${x:,.2f}")
 df_display['Profit'] = df_display['Profit'].apply(lambda x: f"${x:,.2f}")
+#print(df_display.head())
 
+# Question 1: Regression of Profit on all explantory variables
+X = df_clean[['MTenure', 'CTenure', 'Visibility', 'PedCount', 'Res', 'Hours24']]
+X = sm.add_constant(X)
+y = df_clean['Profit']
 
-print(df_display.head())
+model = sm.OLS(y, X).fit() 
+print(model.summary())
